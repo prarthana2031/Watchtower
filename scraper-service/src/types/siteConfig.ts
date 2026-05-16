262d388602a30637b240c44f425bc49dc6d239ac
Prarthana <prarthana6710@gmail.com>
2026-05-16 17:43:41 +0530

    added repectbot and maxrequiests

--- scraper-service/src/types/siteConfig.ts
+++ scraper-service/src/types/siteConfig.ts
@@ -1,8 +1,10 @@
 export interface SiteConfig {
-    name:string;
-    url:string;
-     
-    /** CSS selector that indicates the page has fully loaded (eg., 'nav','sidebar') */
+    name: string;
+    url: string;
 
-    readySelector :string;
+    /** CSS selector that indicates the page has fully loaded (eg., 'nav','sidebar') */
+    readySelector: string;
+
+    respectRobotsTxt?: boolean;
+    maxRequestsPerRun?: number;
 }
